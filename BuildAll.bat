13eef689b5c5d58df4cc2914a210963c5220a412
bmla <detailx@gmail.com>
2016-06-02 14:56:46 +0200

    Changed image sizes for pull techniques

--- BuildAll.bat
+++ BuildAll.bat
@@ -6,14 +6,11 @@
 
 
 
-::Echo Building Litterature
-::START "LitBuilder" /D "%~dp0review" /wait  "BuildLiterature.bat" -nonstop 
-
 Echo Building Paper
 START "PaperBuilder" /D "%~dp0paper" /wait "BuildPaper.bat" -nonstop
 
 Echo Building Appendix
-START "AppendixBuilder" /D "%~dp0appendix" /wait "BuildAppendix.bat" -nonstop
+START "AppendixBuilder" /D "%~dp0docs\appendix" /wait "BuildAppendix.bat" -nonstop
 
 Echo Building Combined PDF
 START "CombinedBuilder"  /D "%~dp0" /wait "BuildCombined.bat" -nonstop
